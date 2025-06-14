a9cf84484aa0911d6bf65ab5c01a76aaf71c4098
xonx4l <sk797119@gmail.com>
2025-06-14 21:15:04 +0530

    update mod.rs

---
+++ src/oms/mod.rs
@@ -0,0 +1,19 @@
+pub mod order;
+pub mod positions;
+
+use tokio::sync::mpsc;
+use tracing::{info, error};
+use std::collection::HashMap;
+use std::sync::Arc;
+use parking_lot::RwLock;
+use anyhow::Result;
+
+#[derive(Debug, Clone)]
+pub enum OmsUpdate{
+    OrderCreated(Order),
+    OrderStateChange{
+        order_id: uuid::Uuid,
+        new_state; OrderState,
+        timestamp: chrono::DateTime::<chrono::Utc>,
+    },
+}
