32cc3be2f4b525902601cb080bceaf1803cbd6d4
xonx4l <sk797119@gmail.com>
2025-07-04 14:36:12 +0530

    add fn run_strategy_engine

--- src/strategy/mod.rs
+++ src/strategy/mod.rs
@@ -7,6 +7,18 @@ pub enum StrategyControl {
     UpdateParams(StrategyConfig),
 }
 
-pub enum strategyEngine {
-    
+pub async fn run_strategy_engine( 
+    mut config: StrategyConfig,
+    mut control_rx: mpsc::UnboundedReceiver<StrategyControl>,
+    _order_tx: mpsc::UnboundedSender<Order>,
+) -> Result<()> {
+    info!("Strategy Engine Started. Config: {:?}", config);
+
+    let mut is_running =  config.enabled;
+
+    loop{
+        tokio::select! {
+
+        }
+    }
 }
